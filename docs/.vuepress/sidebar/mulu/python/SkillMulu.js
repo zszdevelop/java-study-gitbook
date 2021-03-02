3ef22dcb7a72618a9327faf60f4a43ff0d0d0f19
zszdevelop <zszdevelop@163.com>
2021-03-02 17:14:48 +0800

    idea

--- docs/.vuepress/sidebar/mulu/python/SkillMulu.js
+++ docs/.vuepress/sidebar/mulu/python/SkillMulu.js
@@ -7,15 +7,17 @@ exports.mulu = {
         {
             title: '免费短信注册各平台',   // 必要的
             path: 'python/skill/免费短信注册各平台.md'
-        },{
-        title: 'ip代理',   // 必要的
-    //path: '/base/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-    collapsable: true, // 可选的, 默认值是 true,
-    sidebarDepth: 1,    // 可选的, 默认值是 1
-    children: [
-        {
-            title: '小象代理',   // 必要的
-            path: 'python/ipproxy/小象代理.md'
+        }, {
+            title: 'ip代理',   // 必要的
+            //path: '/base/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+            collapsable: true, // 可选的, 默认值是 true,
+            sidebarDepth: 1,    // 可选的, 默认值是 1
+            children: [
+                {
+                    title: '小象代理',   // 必要的
+                    path: 'python/skill/ipproxy/小象代理.md'
+                }
+            ]
         }
     ]
 }
