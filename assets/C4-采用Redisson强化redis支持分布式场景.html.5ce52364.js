4f30b187c60d00b5b6910925c5209a323b385efc
zszdevelop <zszdevelop@zkaola.com>
2022-08-11 15:24:15 +0000

    Deploying to gh-pages from @ zszdevelop/java-study-gitbook@be7cb686b0be111d8c627fe0b7f161f9bc8621f0 🚀

---
+++ assets/C4-采用Redisson强化redis支持分布式场景.html.5ce52364.js
@@ -0,0 +1,12 @@
+import { o as openBlock, c as createElementBlock, e as createStaticVNode } from "./app.f59c6cb9.js";
+import { _ as _export_sfc } from "./plugin-vue_export-helper.21dcd24c.js";
+const _sfc_main = {};
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode('<h1 id="\u91C7\u7528redisson\u5F3A\u5316redis\u652F\u6301\u5206\u5E03\u5F0F\u573A\u666F" tabindex="-1"><a class="header-anchor" href="#\u91C7\u7528redisson\u5F3A\u5316redis\u652F\u6301\u5206\u5E03\u5F0F\u573A\u666F" aria-hidden="true">#</a> \u91C7\u7528Redisson\u5F3A\u5316redis\u652F\u6301\u5206\u5E03\u5F0F\u573A\u666F</h1><h2 id="_1-\u7B80\u4ECB" tabindex="-1"><a class="header-anchor" href="#_1-\u7B80\u4ECB" aria-hidden="true">#</a> 1. \u7B80\u4ECB</h2><p>\u6211\u4EEC\u9879\u76EE\u539F\u672C\u4F7F\u7528redis \u53EA\u4F5C\u4E3A\u7F13\u5B58\u4F7F\u7528\u3002</p><p>\u6211\u4EEC\u91C7\u7528redisson\u5E76\u4E0D\u662F\u66FF\u6362redis\uFF0C\u5E95\u5C42\u8FD8\u662Fredis\u3002\u662F\u5728\u5E94\u7528\u5C42\u91C7\u7528redisson \u652F\u6301\u66F4\u4E30\u5BCC\u7684\u529F\u80FD\u3002\u5982</p><ul><li>\u5206\u5E03\u5F0F\u9501</li><li>\u9650\u6D41</li><li>\u53D1\u5E03\u8BA2\u9605\u6A21\u5F0F</li><li>\u652F\u6301\u96C6\u7FA4</li><li>\u6574\u5408spring-cache</li></ul>', 5);
+const _hoisted_6 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache) {
+  return openBlock(), createElementBlock("div", null, _hoisted_6);
+}
+var C4___Redisson__redis________html = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "C4-\u91C7\u7528Redisson\u5F3A\u5316redis\u652F\u6301\u5206\u5E03\u5F0F\u573A\u666F.html.vue"]]);
+export { C4___Redisson__redis________html as default };
---
+++ assets/Redis缓存预热.html.2d46158b.js
@@ -0,0 +1,12 @@
+import { o as openBlock, c as createElementBlock, e as createStaticVNode } from "./app.f59c6cb9.js";
+import { _ as _export_sfc } from "./plugin-vue_export-helper.21dcd24c.js";
+const _sfc_main = {};
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode('<h1 id="redis\u7F13\u5B58\u9884\u70ED" tabindex="-1"><a class="header-anchor" href="#redis\u7F13\u5B58\u9884\u70ED" aria-hidden="true">#</a> Redis\u7F13\u5B58\u9884\u70ED</h1><h2 id="_1-\u7B80\u4ECB" tabindex="-1"><a class="header-anchor" href="#_1-\u7B80\u4ECB" aria-hidden="true">#</a> 1. \u7B80\u4ECB</h2><p>\u7F13\u5B58\u9884\u70ED\u5C31\u662F\u7CFB\u7EDF\u4E0A\u7EBF\u540E\uFF0C<strong>\u5C06\u76F8\u5173\u7684\u7F13\u5B58\u6570\u636E\u76F4\u63A5\u52A0\u8F7D\u5230\u7F13\u5B58\u7CFB\u7EDF</strong></p><p>\u8FD9\u6837\u53EF\u4EE5\u907F\u514D\u5728\u7528\u6237\u8BF7\u6C42\u7684\u65F6\u5019\uFF0C\u5148\u67E5\u8BE2\u6570\u636E\u5E93\uFF0C\u7136\u540E\u518D\u5C06\u6570\u636E\u7F13\u5B58\u7684\u95EE\u9898\uFF01\u7528\u6237\u76F4\u63A5\u67E5\u8BE2\u4E8B\u5148\u88AB\u9884\u70ED\u7684\u7F13\u5B58\u6570\u636E\uFF01</p><h2 id="_2-\u89E3\u51B3\u65B9\u6848" tabindex="-1"><a class="header-anchor" href="#_2-\u89E3\u51B3\u65B9\u6848" aria-hidden="true">#</a> 2.\u89E3\u51B3\u65B9\u6848</h2><h3 id="_2-1-\u7F13\u5B58\u5237\u65B0\u9875\u9762" tabindex="-1"><a class="header-anchor" href="#_2-1-\u7F13\u5B58\u5237\u65B0\u9875\u9762" aria-hidden="true">#</a> 2.1 \u7F13\u5B58\u5237\u65B0\u9875\u9762</h3><p>\u76F4\u63A5\u5199\u4E2A\u7F13\u5B58\u5237\u65B0\u9875\u9762\uFF0C\u4E0A\u7EBF\u65F6\u624B\u5DE5\u64CD\u4F5C</p><h3 id="_2-2-\u9879\u76EE\u542F\u52A8\u81EA\u52A8\u52A0\u8F7D" tabindex="-1"><a class="header-anchor" href="#_2-2-\u9879\u76EE\u542F\u52A8\u81EA\u52A8\u52A0\u8F7D" aria-hidden="true">#</a> 2.2 \u9879\u76EE\u542F\u52A8\u81EA\u52A8\u52A0\u8F7D</h3><p>\u6570\u636E\u91CF\u4E0D\u5927\uFF0C\u53EF\u4EE5\u5728\u9879\u76EE\u542F\u52A8\u7684\u65F6\u5019\u81EA\u52A8\u8FDB\u884C\u52A0\u8F7D</p><p>\u76EE\u7684\u5C31\u662F\u5728\u7CFB\u7EDF\u4E0A\u7EBF\u524D\uFF0C\u5C06\u6570\u636E\u52A0\u8F7D\u5230\u7F13\u5B58\u4E2D\u3002</p>', 10);
+const _hoisted_11 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache) {
+  return openBlock(), createElementBlock("div", null, _hoisted_11);
+}
+var Redis_____html = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "Redis\u7F13\u5B58\u9884\u70ED.html.vue"]]);
+export { Redis_____html as default };
