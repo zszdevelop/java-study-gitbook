3cb9acd6189891fcf310c7937451f7dbd525af12
zszdevelop <zszdevelop@zkaola.com>
2022-08-18 16:00:20 +0000

    Deploying to gh-pages from @ zszdevelop/java-study-gitbook@bdddff80dc5cdbbdf7766a1e85f64e2d953959ca 🚀

---
+++ assets/C6-form-generator初尝试.html.bc4e0438.js
@@ -0,0 +1,28 @@
+import { r as resolveComponent, o as openBlock, c as createElementBlock, a as createBaseVNode, b as createVNode, e as createStaticVNode, d as createTextVNode } from "./app.46f7153a.js";
+import { _ as _export_sfc } from "./plugin-vue_export-helper.21dcd24c.js";
+const _sfc_main = {};
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode('<h1 id="form-generator\u521D\u5C1D\u8BD5" tabindex="-1"><a class="header-anchor" href="#form-generator\u521D\u5C1D\u8BD5" aria-hidden="true">#</a> form-generator\u521D\u5C1D\u8BD5</h1><h2 id="_1-\u80CC\u666F" tabindex="-1"><a class="header-anchor" href="#_1-\u80CC\u666F" aria-hidden="true">#</a> 1. \u80CC\u666F</h2><p>\u9879\u76EE\u4E2D\u5B58\u5728\u7740\u5927\u91CF\u7684\u8868\u5355\uFF0C\u4E4B\u524D\u7684\u5B9E\u73B0\u90FD\u662F\u9488\u5BF9\u539F\u578B\u9875\u9762\u4E00\u4E2A\u4E2A\u7EC4\u4EF6\u624B\u52A8\u5199\u5165\u3002\u90A3\u4E48\u8FD9\u6837\u4F1A\u9762\u4E34\u7740\u4E0D\u5C11\u95EE\u9898</p><ul><li>\u8868\u5355\u7EC4\u4EF6\u591A\uFF0C\u6837\u5F0F\u5DEE\u522B\u4E0D\u5927\uFF0C\u5199\u4E2A\u9875\u9762\u8017\u65F6\u957F\uFF0C\u4E14\u7E41\u7410</li><li>\u9875\u9762\u98CE\u683C\u4E0D\u597D\u63A7\u5236</li></ul><h2 id="_2-\u5F15\u5165form-generator" tabindex="-1"><a class="header-anchor" href="#_2-\u5F15\u5165form-generator" aria-hidden="true">#</a> 2. \u5F15\u5165form-generator</h2>', 5);
+const _hoisted_6 = /* @__PURE__ */ createTextVNode("\u6211\u4EEC\u4F7F\u7528 ");
+const _hoisted_7 = {
+  href: "https://github.com/JakHuang/form-generator",
+  target: "_blank",
+  rel: "noopener noreferrer"
+};
+const _hoisted_8 = /* @__PURE__ */ createTextVNode("form-generator");
+const _hoisted_9 = /* @__PURE__ */ createStaticVNode('<p><img src="https://zszblog.oss-cn-beijing.aliyuncs.com/zszblog/blogimage-master/68747470733a2f2f616530312e616c6963646e2e636f6d2f6b662f553531626662363631616261393435623438613463373137373434323164343134432e676966.gif" alt="68747470733a2f2f616530312e616c6963646e2e636f6d2f6b662f553531626662363631616261393435623438613463373137373434323164343134432e676966"></p><ul><li>\u901A\u8FC7\u62D6\u62FD\u7684\u65B9\u5F0F\uFF0C\u5FEB\u901F\u8FD8\u539F\u539F\u578B\u9875\u9762</li><li>\u7075\u6D3B\u7684\u63A7\u5236\u8868\u5355\u6837\u5F0F\u7B49</li><li>\u5BFC\u51FA\u6807\u51C6\u7684vue\u4EE3\u7801\uFF0C\u56E2\u961F\u4EE3\u7801\u98CE\u683C\u7EDF\u4E00</li></ul><h2 id="_3-\u5BFC\u51FA\u65B9\u5F0F" tabindex="-1"><a class="header-anchor" href="#_3-\u5BFC\u51FA\u65B9\u5F0F" aria-hidden="true">#</a> 3. \u5BFC\u51FA\u65B9\u5F0F</h2><p>\u5BFC\u51FA\u65B9\u5F0F\u6709vue\u6587\u4EF6\u548Cjson</p><ul><li>Vue <ul><li>\u597D\u5904 <ul><li>\u4EE3\u7801\u7ED3\u6784\u6E05\u6670\uFF0C\u590D\u6838\u5927\u90E8\u5206\u524D\u7AEF\u5F00\u53D1\u8005\u7684\u601D\u7EF4</li></ul></li></ul></li><li>Json <ul><li>\u597D\u5904 <ul><li>\u65B9\u4FBF\u6269\u5C55</li></ul></li><li>\u7F3A\u70B9\uFF1A <ul><li>\u4EE3\u7801\u53EF\u8BFB\u6027\u5DEE</li></ul></li></ul></li></ul>', 5);
+function _sfc_render(_ctx, _cache) {
+  const _component_ExternalLinkIcon = resolveComponent("ExternalLinkIcon");
+  return openBlock(), createElementBlock("div", null, [
+    _hoisted_1,
+    createBaseVNode("p", null, [
+      _hoisted_6,
+      createBaseVNode("a", _hoisted_7, [
+        _hoisted_8,
+        createVNode(_component_ExternalLinkIcon)
+      ])
+    ]),
+    _hoisted_9
+  ]);
+}
+var C6FormGenerator____html = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "C6-form-generator\u521D\u5C1D\u8BD5.html.vue"]]);
+export { C6FormGenerator____html as default };
---
+++ assets/C6-关闭eslint.html.abbc8f9a.js
@@ -0,0 +1,36 @@
+import { r as resolveComponent, o as openBlock, c as createElementBlock, a as createBaseVNode, b as createVNode, e as createStaticVNode, d as createTextVNode } from "./app.46f7153a.js";
+import { _ as _export_sfc } from "./plugin-vue_export-helper.21dcd24c.js";
+const _sfc_main = {};
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode('<h1 id="\u5173\u95EDeslint" tabindex="-1"><a class="header-anchor" href="#\u5173\u95EDeslint" aria-hidden="true">#</a> \u5173\u95EDeslint</h1><h2 id="_1-vue-cli\u811A\u624B\u67B6\u7684\u5173\u95ED\u65B9\u6CD5" tabindex="-1"><a class="header-anchor" href="#_1-vue-cli\u811A\u624B\u67B6\u7684\u5173\u95ED\u65B9\u6CD5" aria-hidden="true">#</a> 1. vue-cli\u811A\u624B\u67B6\u7684\u5173\u95ED\u65B9\u6CD5</h2><p><code>build/webpack.base.conf.js</code> \u914D\u7F6E\u6587\u4EF6\u4E2D\u7684eslint rules\u6CE8\u91CA\u6389</p><div class="language-html ext-html line-numbers-mode"><pre class="language-html"><code>module: {\n    rules: [\n      // {\n      //   test: /\\.(js|vue)$/,\n      //   loader: &#39;eslint-loader&#39;,\n      //   enforce: &#39;pre&#39;,\n      //   include: [resolve(&#39;src&#39;), resolve(&#39;test&#39;)],\n      //   options: {\n      //     formatter: require(&#39;eslint-friendly-formatter&#39;)\n      //   }\n      // },\n      \n      ...\n     ]\n     ...\n   }\n</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>\u5982\u679C\u60F3\u4FDD\u7559eslint\u7684\u8BED\u6CD5\u68C0\u6D4B\uFF0C\u90A3\u5C31\u628A\u4E0D\u7B26\u5408\u81EA\u5DF1\u4E60\u60EF\u7684\u89C4\u5219</p><h2 id="_2-\u5173\u95EDvue-cli3-0-\u7684eslint" tabindex="-1"><a class="header-anchor" href="#_2-\u5173\u95EDvue-cli3-0-\u7684eslint" aria-hidden="true">#</a> 2. \u5173\u95EDvue-cli3.0 \u7684eslint</h2><p>\u5728 vue.config.js\u4E2D\u5C06\u4EE5\u4E0B\u4E09\u9879\u8BBE\u7F6E\u4E3Afalse</p><div class="language-css ext-css line-numbers-mode"><pre class="language-css"><code><span class="token selector">module.exports =</span> <span class="token punctuation">{</span>  \n    <span class="token selector">...\n    // lintOnSave: p<wbr>rocess.env.NODE_ENV === &#39;development&#39;,\n    lintOnSave: false,\n	devServer:</span> <span class="token punctuation">{</span>\n        <span class="token selector">overlay:</span> <span class="token punctuation">{</span>\n            <span class="token property">warnings</span><span class="token punctuation">:</span> false<span class="token punctuation">,</span>\n            <span class="token property">errors</span><span class="token punctuation">:</span> false\n        <span class="token punctuation">}</span>\n    <span class="token punctuation">}</span>\n<span class="token punctuation">}</span>\n</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="\u53C2\u8003\u6587\u7AE0" tabindex="-1"><a class="header-anchor" href="#\u53C2\u8003\u6587\u7AE0" aria-hidden="true">#</a> \u53C2\u8003\u6587\u7AE0</h2>', 9);
+const _hoisted_10 = {
+  href: "https://blog.csdn.net/qq_34645412/article/details/78974413",
+  target: "_blank",
+  rel: "noopener noreferrer"
+};
+const _hoisted_11 = /* @__PURE__ */ createTextVNode("\u5173\u95ED\u4EE4\u4EBA\u6293\u72C2\u7684ESlint \u8BED\u6CD5\u68C0\u6D4B\u914D\u7F6E\u65B9\u6CD5");
+const _hoisted_12 = {
+  href: "https://www.jianshu.com/p/bfc7e7329cff",
+  target: "_blank",
+  rel: "noopener noreferrer"
+};
+const _hoisted_13 = /* @__PURE__ */ createTextVNode("vue-cli3\u7684eslint\u914D\u7F6E\u95EE\u9898");
+function _sfc_render(_ctx, _cache) {
+  const _component_ExternalLinkIcon = resolveComponent("ExternalLinkIcon");
+  return openBlock(), createElementBlock("div", null, [
+    _hoisted_1,
+    createBaseVNode("p", null, [
+      createBaseVNode("a", _hoisted_10, [
+        _hoisted_11,
+        createVNode(_component_ExternalLinkIcon)
+      ])
+    ]),
+    createBaseVNode("p", null, [
+      createBaseVNode("a", _hoisted_12, [
+        _hoisted_13,
+        createVNode(_component_ExternalLinkIcon)
+      ])
+    ])
+  ]);
+}
+var C6___eslint_html = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "C6-\u5173\u95EDeslint.html.vue"]]);
+export { C6___eslint_html as default };
