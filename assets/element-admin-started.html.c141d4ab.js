b020315a4d05e120653fe1e6d68332d6e71b0a0c
zszdevelop <zszdevelop@zkaola.com>
2022-09-18 23:00:42 +0800

    Deploying to gh-pages from @ zszdevelop/java-study-gitbook@066c9cb7dba1cb0dbd5bfef5e3d977ec4de63503 🚀

---
+++ assets/element-admin-started.html.c141d4ab.js
@@ -0,0 +1,31 @@
+import{_ as a}from"./_plugin-vue_export-helper.cdc0426e.js";import{o as r,c as t,a as e,b as d,d as n,e as s,r as l}from"./app.8f09d17c.js";const c={},o=e("h1",{id:"vue-element-admin\u5165\u95E8\u4F7F\u7528",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#vue-element-admin\u5165\u95E8\u4F7F\u7528","aria-hidden":"true"},"#"),n(" vue-element-admin\u5165\u95E8\u4F7F\u7528")],-1),m=e("h2",{id:"_1-\u7B80\u4ECB",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#_1-\u7B80\u4ECB","aria-hidden":"true"},"#"),n(" 1. \u7B80\u4ECB")],-1),v={href:"https://panjiachen.github.io/vue-element-admin",target:"_blank",rel:"noopener noreferrer"},u=n("vue-element-admin"),h=n(" \u662F\u4E00\u4E2A\u540E\u53F0\u524D\u7AEF\u89E3\u51B3\u65B9\u6848\uFF0C\u5B83\u57FA\u4E8E "),_={href:"https://github.com/vuejs/vue",target:"_blank",rel:"noopener noreferrer"},b=n("vue"),p=n(" \u548C "),g={href:"https://github.com/ElemeFE/element",target:"_blank",rel:"noopener noreferrer"},x=n("element-ui"),f=n("\u5B9E\u73B0\u3002\u5B83\u4F7F\u7528\u4E86\u6700\u65B0\u7684\u524D\u7AEF\u6280\u672F\u6808\uFF0C\u5185\u7F6E\u4E86 i18n \u56FD\u9645\u5316\u89E3\u51B3\u65B9\u6848\uFF0C\u52A8\u6001\u8DEF\u7531\uFF0C\u6743\u9650\u9A8C\u8BC1\uFF0C\u63D0\u70BC\u4E86\u5178\u578B\u7684\u4E1A\u52A1\u6A21\u578B\uFF0C\u63D0\u4F9B\u4E86\u4E30\u5BCC\u7684\u529F\u80FD\u7EC4\u4EF6\uFF0C\u5B83\u53EF\u4EE5\u5E2E\u52A9\u4F60\u5FEB\u901F\u642D\u5EFA\u4F01\u4E1A\u7EA7\u4E2D\u540E\u53F0\u4EA7\u54C1\u539F\u578B\u3002"),k=s(`<p>\u4E00\u5957\u5B8C\u5584\u7684\u540E\u53F0\u7BA1\u7406\u7CFB\u7EDF\u7684\u524D\u7AEF\u89E3\u51B3\u65B9\u6848\u3002\u5DF2\u7ECF\u5185\u7F6E\u4E86\u4E30\u5BCC\u7684\u529F\u80FD\u3002\u53EF\u5FEB\u901F\u6539\u9020\u5F00\u53D1\u4E0A\u624B</p><h2 id="_2-demo\u8FD0\u884C" tabindex="-1"><a class="header-anchor" href="#_2-demo\u8FD0\u884C" aria-hidden="true">#</a> 2. demo\u8FD0\u884C</h2><h3 id="_2-1-\u5F00\u53D1" tabindex="-1"><a class="header-anchor" href="#_2-1-\u5F00\u53D1" aria-hidden="true">#</a> 2.1 \u5F00\u53D1</h3><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code># \u514B\u9686\u9879\u76EE
+git clone https://github.com/PanJiaChen/vue-element-admin.git
+
+# \u8FDB\u5165\u9879\u76EE\u76EE\u5F55
+cd vue-element-admin
+
+# \u5B89\u88C5\u4F9D\u8D56
+npm install
+
+# \u5EFA\u8BAE\u4E0D\u8981\u76F4\u63A5\u4F7F\u7528 cnpm \u5B89\u88C5\u4F9D\u8D56\uFF0C\u4F1A\u6709\u5404\u79CD\u8BE1\u5F02\u7684 bug\u3002\u53EF\u4EE5\u901A\u8FC7\u5982\u4E0B\u64CD\u4F5C\u89E3\u51B3 npm \u4E0B\u8F7D\u901F\u5EA6\u6162\u7684\u95EE\u9898
+npm install --registry=https://registry.npm.taobao.org
+
+# \u542F\u52A8\u670D\u52A1
+npm run dev
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,4),E=n("\u6D4F\u89C8\u5668\u8BBF\u95EE "),N={href:"http://localhost:9527/",target:"_blank",rel:"noopener noreferrer"},V=n("http://localhost:9527"),B=s(`<h3 id="_2-2-\u53D1\u5E03" tabindex="-1"><a class="header-anchor" href="#_2-2-\u53D1\u5E03" aria-hidden="true">#</a> 2.2 \u53D1\u5E03</h3><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code># \u6784\u5EFA\u6D4B\u8BD5\u73AF\u5883
+npm run build:stage
+
+# \u6784\u5EFA\u751F\u4EA7\u73AF\u5883
+npm run build:prod
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="_2-3-\u5176\u5B83" tabindex="-1"><a class="header-anchor" href="#_2-3-\u5176\u5B83" aria-hidden="true">#</a> 2.3 \u5176\u5B83</h3><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code># \u9884\u89C8\u53D1\u5E03\u73AF\u5883\u6548\u679C
+npm run preview
+
+# \u9884\u89C8\u53D1\u5E03\u73AF\u5883\u6548\u679C + \u9759\u6001\u8D44\u6E90\u5206\u6790
+npm run preview -- --report
+
+# \u4EE3\u7801\u683C\u5F0F\u68C0\u67E5
+npm run lint
+
+# \u4EE3\u7801\u683C\u5F0F\u68C0\u67E5\u5E76\u81EA\u52A8\u4FEE\u590D
+npm run lint -- --fix
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,4);function j(w,y){const i=l("ExternalLinkIcon");return r(),t("div",null,[o,m,e("blockquote",null,[e("p",null,[e("a",v,[u,d(i)]),h,e("a",_,[b,d(i)]),p,e("a",g,[x,d(i)]),f])]),k,e("p",null,[E,e("a",N,[V,d(i)])]),B])}const L=a(c,[["render",j],["__file","element-admin-started.html.vue"]]);export{L as default};
